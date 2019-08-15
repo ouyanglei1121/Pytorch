2b4bb2007260f4f5882932c58c9b1b3e79688276
oyl <1121699682qq.com>
2019-08-15 15:13:17 +0800

    add  day04

--- day04/demo02/__init__.py
+++ day04/demo02/__init__.py
@@ -3,6 +3,24 @@ Where     Gather
 """
 import torch
 """where"""
+cond = torch.tensor([[0.122, 0.322, 0.623],
+                     [0.544, 0.833, 0.343]])
+a = torch.ones(2, 3)
+b = torch.ones(2, 3) * 3
+x = torch.where(cond > 0.5, a, b)
+print(x)
+
+print("""----------gather(收集) 查表操作的过程-----------""")
+prob = torch.randn(4, 10)
+print(prob)
+idx = prob.topk(dim=0, k=2)
+print(idx)
+idx = idx[1]
+print(idx)
+label = torch.arange(10) + 100
+print(label)
+print(torch.gather(label.expand(4, 10), dim=0, index=idx.long()))
+
 
 
 
